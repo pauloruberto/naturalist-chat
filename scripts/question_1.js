94d6cda613d32f9eb7c8fbd0f3f26c0c54bb0ccd
Paulo <p.ruberto@me.com>
2017-03-10 09:02:52 -0500

    Impoved layout of the question page.

--- scripts/question_1.js
+++ scripts/question_1.js
@@ -1,22 +1,21 @@
 var app = angular.module("question_1", []);
 
-// DROPDOWN SELECTOR
 app.controller("answers", function($scope) {
     $scope.responses = [
         {
-            name: 'Darwin',
+            name: 'Charles Darwin',
             pic: '/images/darwin.png',
             answer: 'This is where his answer would go...',
             likes: '2'
         },
         {
-            name: 'Cuvier',
+            name: 'Georges Cuvier',
             pic: '/images/cuvier.png',
             answer: 'This is where his answer would go...',
             likes: '4'
         },
         {
-            name: 'Lamarck',
+            name: 'Jean-Baptiste Lamarck',
             pic: '/images/lamarck.png',
             answer: 'This is where his answer would go...',
             likes: '6'
@@ -26,6 +25,19 @@ app.controller("answers", function($scope) {
             pic: '/images/lamark.png',
             answer: 'This is where the answer would go...',
             likes: '3'
+        }
+    ];
+});
+
+app.controller("sidebar", function($scope) {
+    $scope.questions = [
+        {
+            id: 'q1',
+            question: 'What is the purpose of the fur?',
+        },
+        {
+            id: 'q2',
+            question: 'Why is the fur seal communal?',
         }
     ];
 });
